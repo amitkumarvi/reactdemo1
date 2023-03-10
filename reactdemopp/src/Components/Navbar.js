df03f63c7129118b7b234626cab20dc2888dc6bd
Amitkumar Mewalal Vishwakarma <amitkumarv@cybage.com>
2023-03-10 12:19:22 +0530

    bootstrap added

--- reactdemopp/src/Components/Navbar.js
+++ reactdemopp/src/Components/Navbar.js
@@ -1,18 +1,55 @@
-import React from 'react'
-import {Link} from 'react-router-dom'
-const Navbar = () => { 
-     return (
-        <div>
-            <Link to="/">Home </Link>
-            <Link to="/About">About Us </Link>
-            <Link to="/Contact">Contact</Link>
-            <Link to="/Login">Log In</Link>
-            <Link to="/Signup">Sign Up</Link>
+import React from "react";
+import { Link } from "react-router-dom";
+const Navbar = () => {
+  return (
+    <div>
+      <nav class="navbar navbar-expand-lg bg-body-tertiary">
+        <div class="container-fluid">
+          <button
+            class="navbar-toggler"
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="#navbarSupportedContent"
+            aria-controls="navbarSupportedContent"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <span class="navbar-toggler-icon"></span>
+          </button>
+          <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+              <li class="nav-item">
+                <Link to="/" class="nav-link active">
+                  Home
+                </Link>
+              </li>
+              <li class="nav-item">
+                <Link to="/About" class="nav-link">
+                  About Us
+                </Link>
+              </li>
 
-
-
+              <li class="nav-item">
+                <Link to="/Contact" class="nav-link">
+                  Contact
+                </Link>
+              </li>
+              <li class="nav-item">
+                <Link to="/Login" class="nav-link">
+                  Log In
+                </Link>
+              </li>
+              <li class="nav-item">
+                <Link to="/Signup" class="nav-link">
+                  Sign Up
+                </Link>
+              </li>
+            </ul>
           </div>
-         )
-}
+        </div>
+      </nav>
+    </div>
+  );
+};
 
-export default Navbar
+export default Navbar;
