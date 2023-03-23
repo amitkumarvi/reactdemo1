35fb7ef382834a780c624d0355d98cb1d9af69a5
Amitkumar Mewalal Vishwakarma <amitkumarv@cybage.com>
2023-03-23 18:32:59 +0530

    practice counter in redux

--- reactdemopp/src/Components/Navbar.js
+++ reactdemopp/src/Components/Navbar.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { Link } from "react-router-dom";
 const Navbar = () => {
+  
   return (
     <div>
       <nav class="navbar navbar-expand-lg bg-body-tertiary">
--- reactdemopp/src/Pages/Home.js
+++ reactdemopp/src/Pages/Home.js
@@ -1,9 +1,27 @@
 import React from 'react'
+import { useSelector, useDispatch } from 'react-redux'
+import { actions } from '../store'
+
 
 const Home = () => { 
-     return (
+const counter = useSelector((state)=>state.counter);
+const dispatch = useDispatch(actions.increment());
+
+const increment = () =>{
+    dispatch(actions.increment());
+}
+
+const decrement = () =>{
+    dispatch(actions.decrement());
+}
+
+
+
+    return (
          <div>
-            <h2>Home</h2>
+                <h2>{counter}</h2>        
+                <button onClick={increment}>click me</button>    
+                <button onClick={decrement}>click me</button>    
         </div>  
          )
 }
